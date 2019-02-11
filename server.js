9010854ea919dd7cc810792be9cb477b337da207
Craig Jensen <cjensen@microsoft.com>
2019-02-10 17:53:04 -0800

    test

--- server.js
+++ server.js
@@ -51,21 +51,22 @@ var   displayHistory = []
 // kick off the scaler
 //scale.sync(options);
 
+const threads = [];
+
 if (options.threads > 1)
 {
     console.log("spawning " + options.threads + " worker threads");
 
     for (i=0; i < options.threads; i++) {
-        thread = spawn(function(input, done) {
+        console.log("thread " + i + " spawning");
+        threads.push(spawn(function(input, done) {
             // Everything we do here will be run in parallel in another execution context.
             // Remember that this function will be executed in the thread's context,
             // so you cannot reference any value of the surrounding code.
             console.log("thread " + i + " running");
             scale.sync3(options)
             done({ string : input.string, integer : parseInt(input.string) });
-        });
-
-        thread
+        })
         //.on('message', function(response) {
         //    console.log('123 * 2 = ', response.integer * 2);
         //    thread.kill();
@@ -75,7 +76,7 @@ if (options.threads > 1)
         })
         .on('exit', function() {
             console.log('Worker has been terminated.');
-        });
+        }))
     }
 }
 
